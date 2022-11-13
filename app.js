84e076ed3e993c655b8a536d561f390158f04a01
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-11-13 21:52:49 +0100

    added submit post form with handler

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ app.get('/contact', (req, res) => {
   res.render('contact', { content: contactContent });
 });
 
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/compose', ({ body: { postTitle } }) => {
+  console.log(postTitle);
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
