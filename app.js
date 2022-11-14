29c2a6d3db9d98bd0017d6608b1a38fd58f1a1dd
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-11-14 06:39:24 +0100

    save posts to local array

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const posts = [];
 const homeStartingContent =
   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
 const aboutContent =
@@ -16,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('home', { content: homeStartingContent });
+  res.render('home', { content: homeStartingContent, posts });
 });
 
 app.get('/about', (req, res) => {
@@ -31,8 +32,10 @@ app.get('/compose', (req, res) => {
   res.render('compose');
 });
 
-app.post('/compose', ({ body: { postTitle } }) => {
-  console.log(postTitle);
+app.post('/compose', ({ body: { postTitle, postBody } }, res) => {
+  const post = { title: postTitle, body: postBody };
+  posts.push(post);
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
