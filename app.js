697f7c7e72e133f109dec48dbd848551b5d312b7
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-11-23 20:40:46 +0100

    user registration

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { get } = require('lodash/fp');
 const Post = require('./model/post');
+const User = require('./model/user');
 
 const homeStartingContent =
   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
@@ -66,6 +67,16 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
   res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const user = get(['body'], req);
+  const newUser = new User(user);
+  newUser.save((err) => {
+    if (!err) {
+      res.redirect('/');
+    }
+  });
 });
 
 app.listen(3000, () => {
